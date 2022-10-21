a0f7d821777bf3ab390b4555b9f1e100adf9c6f8
James Fabe <jaafaber@gmail.com>
2022-10-20 23:53:51 -0600

    added a three second context timeout for the queries

--- internal/data/todo.go
+++ internal/data/todo.go
@@ -3,6 +3,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"time"
@@ -39,12 +40,17 @@ func (m TodoModel) Insert(todo *Todo) error {
 		VALUE ($1, $2)
 		RETURNING id, created_at, version
 	`
+	// Create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Cleanup to prevent memory leaks
+	defer cancel()
+
 	//colllect the data fields into a slice
 	args := []interface{}{
 		todo.Name,
 		todo.Task,
 	}
-	return m.DB.QueryRow(query, args...).Scan(&todo.ID, &todo.CreatedAt, &todo.Version)
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&todo.ID, &todo.CreatedAt, &todo.Version)
 }
 
 // Get() allows us to retrieve a specific task
@@ -55,15 +61,20 @@ func (m TodoModel) Get(id int64) (*Todo, error) {
 	}
 	// Create the query
 	query := `
-		SELECT id, created_at, name, task, version
+		SELECT  id, created_at, name, task, version
 		FROM todo
 		WHERE id = $1
 	`
 	// Declare a todo variable to hold the returned data
 	var todo Todo
 
+	// Create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Cleanup to prevent memory leaks
+	defer cancel()
+
 	// Execute the query using QueryRow()
-	err := m.DB.QueryRow(query, id).Scan(
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
 		&todo.ID,
 		&todo.CreatedAt,
 		&todo.Name,
@@ -94,6 +105,11 @@ func (m TodoModel) Update(todo *Todo) error {
 		AND version = $4
 		RETURNING version
 	`
+	// Create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Cleanup to prevent memory leaks
+	defer cancel()
+
 	args := []interface{}{
 		todo.Name,
 		todo.Task,
@@ -101,7 +117,7 @@ func (m TodoModel) Update(todo *Todo) error {
 	}
 
 	// Check for edit conflicts
-	err := m.DB.QueryRow(query, args...).Scan(&todo.Version)
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&todo.Version)
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -124,8 +140,13 @@ func (m TodoModel) Delete(id int64) error {
 		DELETE FROM todo
 		WHERE id = $1
 	`
+	// Create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Cleanup to prevent memory leaks
+	defer cancel()
+
 	// Execute the query
-	result, err := m.DB.Exec(query, id)
+	result, err := m.DB.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
 	}
